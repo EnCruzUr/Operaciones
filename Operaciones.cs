fc771e7b8b78c3af867e472e16c15881f6c7ffc4
EnCruzUr <cruz_d.3@hotmail.com>
2014-09-23 23:27:23 -0500

    Console
    Agregando Comandos

--- Operaciones.cs
+++ Operaciones.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-
 namespace Operaciones
 {
 	class Program
 	{
 		public static void Main(string[] args)
 		{
-			//Console.WriteLine("Hello World!");
-			
-			// TODO: Implement Functionality Here
-			
+            Console.WriteLine("Este programa Realiza la Suma y Resta de dos numeros que acontinuacion colocaras");
+            Console.WriteLine("Ingresa Primer Valor");
+            Console.WriteLine("Ingresa Segundo Valor");
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
 		}
