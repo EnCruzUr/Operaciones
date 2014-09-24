d8657ea770f9eefd51f21364d7d4f2ac8c63439e
EnCruzUr <cruz_d.3@hotmail.com>
2014-09-23 23:24:51 -0500

    Comentarizacion

--- Operaciones.cs
+++ Operaciones.cs
@@ -6,7 +6,7 @@ namespace Operaciones
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			//Console.WriteLine("Hello World!");
 			
 			// TODO: Implement Functionality Here
 			
