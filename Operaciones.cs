22e7c2e6de1bd8053f61e87fe0146f8d96ef0b21
EnCruzUr <cruz_d.3@hotmail.com>
2014-09-23 23:38:45 -0500

    Agregando Procedimientos
    Se Agrega lo que el programa debe de hacer

--- Operaciones.cs
+++ Operaciones.cs
@@ -3,13 +3,27 @@ namespace Operaciones
 {
 	class Program
 	{
-		public static void Main(string[] args)
-		{
+        public static void Oper(){
+            int N1, N2, R1, R2;
             Console.WriteLine("Este programa Realiza la Suma y Resta de dos numeros que acontinuacion colocaras");
             Console.WriteLine("Ingresa Primer Valor");
+            N1=int.Parse(Console.ReadLine();
             Console.WriteLine("Ingresa Segundo Valor");
-			Console.Write("Press any key to continue . . . ");
+            N2=int.Parse(Console.ReadLine();
+            R1=N1+N2;
+            R2=N1-N2;
+            Console.WriteLine("El Resultado de la Suma es: "+R1);
+            Console.WriteLine("El Resultado de la Resta es: "+R2);
+			Console.Write("\n\nPress any key to continue . . . ");
 			Console.ReadKey(true);
+        
+        
+        }
+
+
+		public static void Main(string[] args)
+		{
+            Oper();
 		}
 	}
 }
